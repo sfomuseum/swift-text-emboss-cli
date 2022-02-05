4ab61f864171d7de44616f3c46c2b3864be901e6
thisisaaronland <aaron@localhost>
2022-02-04 18:01:58 -0800

    basic docs

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # text-cli
 
-A description of this package.
+Command line tool for extracting text from images using Apple's Vision framework.
+
+_`text-cli` is kind of a dumb name but I didn't actually think I would finish this on the first go. This tool (and package) will probably be renamed._
+
+## Example
+
+Both of the images in these examples are included in the [fixtures](fixtures) directory.
+
+```
+$> swift build
+
+$> ./.build/debug/text-cli ./fixtures/sfomuseum-pin-2019.081.021.jpg 
+SANDY HERRMANN
+
+$> ./.build/debug/text-cli ./fixtures/sfomuseum-menu-2000.058.1457.jpg 
+GUAM
+WAKE -GUAM
+LUNCHEON
+Entrées
+Cold Luncheon Buffet of Lobster Bellevue, Beef Wellington and Smoked Breast of Turkey
+Salad a la Russe or Waldorf Salad
+A Selection of Luncheon Breads
+Desserts
+Fruit Flavored Ice Cream Served with Gaufrettes
+proute Guam. Captain Musick radioed from the flight deck,
+Assorted Fresh Fruit and French Pastries
+Everything was functioning perfectly and the China Clipper
+settled down to the long .500-r drive ahead. The cloud
+International Coffee
+moisture on the wings flashed every color of the spectrum and
+Café Parisienne
+Café Royale
+Irish Coffee
+the propellers described great glistening arcs as we sped ahead
+with Grand Marier
+Collee and Cognac
+with Baileys Irish Cream
+a world al to ourselves - except for the nine radio stations and the three
+radio compass stations that kept their invisible fingers on us.
+Italian Coffee
+Café Mexicano
+with Amaretto di Saronno
+The China Clipper crossed the International Date Line and arrived in
+Coffee and Kahlua
+Guam 45 minutes ahead of schedule. After a day of rest and Thanksgiving
+Cappuccino
+dinner at the Governor's Palace, the crew of the China Clipper was ready
+with Chocola
+to embark on the final leg of their journey.
+/ Cognac
+```
+
+## See also
+
+* https://developer.apple.com/documentation/vision
+* https://collection.sfomuseum.org/objects/1762939445/
+* https://collection.sfomuseum.org/objects/1511924565/#1729566967
--- Sources/text-cli/main.swift
+++ Sources/text-cli/main.swift
@@ -3,48 +3,77 @@ import ArgumentParser
 import Vision
 import AppKit
 
+public enum Errors: Error {
+    case notFound
+    case invalidImage
+    case cgImage
+    case processError
+    case unsupportedOS
+}
+
 @available(macOS 10.15, *)
 struct TextExtractCLI: ParsableCommand {
 
-    @Argument(help:"...")
+    @Argument(help:"The path to an image file to extract text from")
     var inputFile: String
     
-    func processImage(image: CGImage) {
+    func processImage(image: CGImage) -> Result<String, Error> {
                 
-        let textRecognitionRequest = VNRecognizeTextRequest()
+        let req = VNRecognizeTextRequest()
         let handler = VNImageRequestHandler(cgImage: image, options: [:])
 
         do {
-            try handler.perform([textRecognitionRequest])
+            try handler.perform([req])
         } catch {
-            print(error)
+            return .failure(error)
         }
+        
+        var transcript = ""
+
+        if req.results != nil {
+            
+            let maximumCandidates = 1
+            for observation in req.results! {
+                guard let candidate = observation.topCandidates(maximumCandidates).first else { continue }
+                transcript += candidate.string
+                transcript += "\n"
+            }
+        }
+
+        transcript = transcript.trimmingCharacters(in: .whitespacesAndNewlines)
+        return .success(transcript)
     }
     
-    func run() {
+    func run() throws {
         
         let fm = FileManager.default
         
         if (!fm.fileExists(atPath: inputFile)){
-            print("File does not exist")
-            return
+            throw(Errors.notFound)
         }
         
         guard let im = NSImage(byReferencingFile:inputFile) else {
-            print("SAD IMAGE")
-            return
+            throw(Errors.invalidImage)
         }
         
-        print("WUB \(inputFile) \(im)")
+        guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            throw(Errors.cgImage)
+        }
+        
+        let rsp = processImage(image: cgImage)
+        
+        switch rsp {
+        case .failure(let error):
+            throw(error)
+        case .success(let txt):
+            print(txt)
+        }
+        
     }
 }
 
 if #available(macOS 10.15, *) {
-    
     TextExtractCLI.main()
-    exit(0)
-    
 } else {
-    print("SAD")
-    exit(1)
+    throw(Errors.unsupportedOS)
 }
